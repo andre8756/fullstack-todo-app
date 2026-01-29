f29d319ebeabe8133c68d2edffa74fd2c01122d7
Adnré Herberto Schmitt <schmitt.andre23@gmail.com>
2026-01-29 20:17:11 -0300

    add TaskService and method creat

--- Back-End/todo/src/main/java/com/todo/Service/TaskService.java
+++ Back-End/todo/src/main/java/com/todo/Service/TaskService.java
@@ -1,8 +1,24 @@
 package com.todo.Service;
 
-
+import com.todo.Dto.TaskRequestDTO;
+import com.todo.Dto.TaskResponseDTO;
 import com.todo.Entity.Task;
+import com.todo.Mapper.TaskMapper;
+import com.todo.Repository.TaskRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class TaskService {
+
+    @Autowired
+    private TaskRepository taskRepository;
+
+    public TaskResponseDTO create(TaskRequestDTO dto){
+        Task task = TaskMapper.toEntity(dto);
+        Task savedTask = taskRepository.save(task);
+
+        return TaskMapper.toDTO(savedTask);
+    }
 
 }
