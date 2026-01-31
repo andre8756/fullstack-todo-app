29766ddc4de593b369072576478f3cda72d3de40
Adnré Herberto Schmitt <schmitt.andre23@gmail.com>
2026-01-31 09:11:13 -0300

    security complete +-

--- Back-End/todo/src/main/java/com/todo/Security/SecurityConfig.java
+++ Back-End/todo/src/main/java/com/todo/Security/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.todo.Security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -10,8 +11,8 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class SecurityConfig {
 
+    @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity){
-
         return httpSecurity
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
